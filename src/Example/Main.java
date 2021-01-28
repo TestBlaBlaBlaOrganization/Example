76715a40e4c11fbad94d17b2c3c2e4b9d0940eb8
GrishaninVyacheslav <64549372+GrishaninVyacheslav@users.noreply.github.com>
2021-01-28 18:26:06 +0300

    deepPrint

--- src/Example/Main.java
+++ src/Example/Main.java
@@ -10,7 +10,11 @@ public class Main {
                 {{(short)1, (short)2}, {(short)3, (short)4}},
                 {{(short)5, (short)6}, {(short)7, (short)8}},
         };
-        PrintArr.deepPrint(arr);
+        int[][][][] matrix = {
+                {{{1, 2}, {3, 4}}, {{5, 6}, {7, 8}}},
+                {{{9, 10}, {11, 12}}, {{13, 14}, {15, 16}}}
+        };
+        PrintArr.deepPrint(matrix);
     }
     // Можно попробовать шаблоны
 }
--- src/Example/PrintArr.java
+++ src/Example/PrintArr.java
@@ -1,91 +1,44 @@
 package Example;
 
 public class PrintArr {
-//    private static void printArr(Object[] arr) {
-//        // System.out.println("#1");
-//
-//        for (Object obj : arr)
-//            System.out.print(obj + " ");
-//    }
-//
-//    public static void printArr(int[] arr) {
-//        //System.out.println("#2");
-//        Integer[] objArr = new Integer[arr.length];
-//        for (int i = 0; i < objArr.length; i++)
-//            objArr[i] = arr[i];
-//        printArr(objArr);
-//    }
-
-    public static void deepPrint(Object obj) {
-        //System.out.println("NAME: " + obj.getClass());
+    public static void deepPrint(Object obj){
+        deepPrint(obj, 0);
+    }
+    private static void deepPrint(Object obj, int recursionLevel) {
         if(obj.getClass().toString().split(" ")[0].equals("class") && obj.getClass().toString().split(" ")[1].split("")[0].equals("["))
             try {
                 for(Object arr: (Object[])obj)
-                    deepPrint(arr);
+                    deepPrint(arr, recursionLevel + 1);
+                System.out.println();
             }catch (ClassCastException e){
                 String type = obj.getClass().toString().split(" ")[1].split("")[1];
                 if(type.equals("B"))
                     for(byte arr: (byte[])obj)
-                        deepPrint(arr);
+                        deepPrint(arr, recursionLevel + 1);
                 else if(type.equals("S"))
                     for(short arr: (short[])obj)
-                        deepPrint(arr);
+                        deepPrint(arr, recursionLevel + 1);
                 else if(type.equals("I"))
                     for(int arr: (int[])obj)
-                        deepPrint(arr);
+                        deepPrint(arr, recursionLevel + 1);
                 else if(type.equals("L"))
                     for(long arr: (long[])obj)
-                        deepPrint(arr);
+                        deepPrint(arr, recursionLevel + 1);
                 else if(type.equals("F"))
                     for(float arr: (float[])obj)
-                        deepPrint(arr);
+                        deepPrint(arr, recursionLevel + 1);
                 else if(type.equals("D"))
                     for(double arr: (double[])obj)
-                        deepPrint(arr);
+                        deepPrint(arr, recursionLevel + 1);
                 else if(type.equals("B"))
                     for(boolean arr: (boolean[])obj)
-                        deepPrint(arr);
+                        deepPrint(arr, recursionLevel + 1);
                 else if(type.equals("C"))
                     for(char arr: (char[])obj)
-                        deepPrint(arr);
+                        deepPrint(arr, recursionLevel + 1);
+                System.out.println();
             }
         else
-            System.out.print(obj + " ");
-
-
-
-
-
-        //System.out.println("#3");
-        //System.out.println(arr.getClass().toString().split(" ")[0].equals("class"));
-        //System.out.println(arr.getClass().toString().split(" ")[1].split("")[0].equals("["));
-//        if (arr.getClass().toString().split(" ")[1].split("")[0].equals("[")) {
-//            try {
-//                Object[][] c = (Object[][]) arr;
-//
-//
-//                int[] itc = ( (int[])(c[0][0]) );
-//
-//
-//
-//                System.out.println("cast");
-//                System.out.println( itc[0] );
-//                // Если не примитивный тип
-//                for (Object[] objArr : c) {
-//                    printArr(objArr);
-//                    System.out.println();
-//                }
-//
-//            } catch (ClassCastException e) { // если примитивный тип
-//                System.out.println("ClassCastException");
-//                if (e.getMessage().split(" ")[1].equals("[[I"))
-//                    for (int[] intArr : (int[][]) arr) {
-//                        printArr(intArr);
-//                        System.out.println();
-//                    }
-//
-//            }
-//        }
+            System.out.print(obj + ", ");
     }
-
 }
