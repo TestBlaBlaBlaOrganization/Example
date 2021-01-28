800894bb5fe73a3fff24fcdd12d0231cb0ac153d
GrishaninVyacheslav <64549372+GrishaninVyacheslav@users.noreply.github.com>
2021-01-28 19:42:50 +0300

    deepPrintFinall

--- src/Example/Main.java
+++ src/Example/Main.java
@@ -3,7 +3,7 @@ package Example;
 public class Main {
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4};
+        int[] arr = {1, 2, 3, 4};
 //        printArr(arr);
         int[][] matrixA = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         short[][][] matrixB = {
--- src/Example/PrintArr.java
+++ src/Example/PrintArr.java
@@ -1,12 +1,13 @@
 package Example;
 
 public class PrintArr {
+
+
     public static void deepPrint(Object obj) {
         deepPrint(obj, 0, false);
     }
 
     private static void deepPrint(Object obj, int recursionLevel, boolean isLast) {
-
         if (obj.getClass().toString().split(" ")[0].equals("class") && obj.getClass().toString().split(" ")[1].split("")[0].equals("["))
             try {
                 if (recursionLevel > 0)
@@ -14,17 +15,15 @@ public class PrintArr {
                 for (int i = 0; i < recursionLevel; i++)
                     System.out.print("  ");
                 System.out.print("[");
-
                 Object[] arr = (Object[]) obj;
                 for (int i = 0; i < arr.length; i++)
                     deepPrint(arr[i], recursionLevel + 1, i == arr.length - 1);
-
-
-                System.out.println();
-                for (int i = 0; i < recursionLevel; i++)
-                    System.out.print("  ");
+                if (arr.getClass().toString().split(" ")[1].split("")[1].equals("[")) {
+                    System.out.println();
+                    for (int i = 0; i < recursionLevel; i++)
+                        System.out.print("  ");
+                }
                 System.out.print("]" + ((!isLast && recursionLevel > 0) ? "," : ""));
-
             } catch (ClassCastException e) {
                 String type = obj.getClass().toString().split(" ")[1].split("")[1];
                 if (type.equals("B")) {
@@ -39,7 +38,7 @@ public class PrintArr {
                     int[] arr = (int[]) obj;
                     for (int i = 0; i < arr.length; i++)
                         deepPrint(arr[i], recursionLevel + 1, i == arr.length - 1);
-                } else if (type.equals("L")) {
+                } else if (type.equals("J")) {
                     long[] arr = (long[]) obj;
                     for (int i = 0; i < arr.length; i++)
                         deepPrint(arr[i], recursionLevel + 1, i == arr.length - 1);
@@ -51,7 +50,7 @@ public class PrintArr {
                     double[] arr = (double[]) obj;
                     for (int i = 0; i < arr.length; i++)
                         deepPrint(arr[i], recursionLevel + 1, i == arr.length - 1);
-                } else if (type.equals("B")) {
+                } else if (type.equals("Z")) {
                     boolean[] arr = (boolean[]) obj;
                     for (int i = 0; i < arr.length; i++)
                         deepPrint(arr[i], recursionLevel + 1, i == arr.length - 1);
@@ -64,6 +63,5 @@ public class PrintArr {
             }
         else
             System.out.print(obj + ((!isLast && recursionLevel > 0) ? ", " : ""));
-
     }
 }
