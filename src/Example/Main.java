11992704c4a38e4609101f6e4ef0e5cb51f30046
GrishaninVyacheslav <64549372+GrishaninVyacheslav@users.noreply.github.com>
2021-01-28 17:10:16 +0300

    we

--- src/Example/Main.java
+++ src/Example/Main.java
@@ -1,18 +1,16 @@
 package Example;
 
-import java.lang.reflect.Array;
-
 public class Main {
 
     public static void main(String[] args) {
 //        int[] arr = {1,2,3,4};
 //        printArr(arr);
-          int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-//        int[][][] matrix = {
-//                {{1, 2}, {3, 4}},
-//                {{5, 6}, {7, 8}},
-//        };
-        PrintArr.printArr(matrix);
+//          int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][][] matrix = {
+                {{1, 2}, {3, 4}},
+                {{5, 6}, {7, 8}},
+        };
+        PrintArr.print(matrix);
     }
     // Можно попробовать шаблоны
 }
--- src/Example/PrintArr.java
+++ src/Example/PrintArr.java
@@ -1,41 +1,68 @@
 package Example;
 
 public class PrintArr {
-    private static void printArr(Object[] arr) {
-        // System.out.println("#1");
+//    private static void printArr(Object[] arr) {
+//        // System.out.println("#1");
+//
+//        for (Object obj : arr)
+//            System.out.print(obj + " ");
+//    }
+//
+//    public static void printArr(int[] arr) {
+//        //System.out.println("#2");
+//        Integer[] objArr = new Integer[arr.length];
+//        for (int i = 0; i < objArr.length; i++)
+//            objArr[i] = arr[i];
+//        printArr(objArr);
+//    }
 
-        for (Object obj : arr)
+    public static void print(Object obj) {
+        //System.out.println("NAME: " + obj.getClass());
+        if(obj.getClass().toString().split(" ")[0].equals("class") && obj.getClass().toString().split(" ")[1].split("")[0].equals("["))
+            try {
+                for(Object arr: (Object[])obj)
+                    print(arr);
+            }catch (ClassCastException e){
+                for(int arr: (int[])obj)
+                    print(arr);
+            }
+        else
             System.out.print(obj + " ");
-    }
 
-    public static void printArr(int[] arr) {
-        //System.out.println("#2");
-        Integer[] objArr = new Integer[arr.length];
-        for (int i = 0; i < objArr.length; i++)
-            objArr[i] = arr[i];
-        printArr(objArr);
-    }
 
-    public static void printArr(Object arr) {
+
+
+
         //System.out.println("#3");
         //System.out.println(arr.getClass().toString().split(" ")[0].equals("class"));
         //System.out.println(arr.getClass().toString().split(" ")[1].split("")[0].equals("["));
-        if (arr.getClass().toString().split(" ")[1].split("")[0].equals("[")) {
-            try {
-                Object[][] c = (Object[][]) arr;
-                for (Object[] objArr : c) {
-                    printArr(objArr);
-                    System.out.println();
-                }
-
-            } catch (ClassCastException e) {
-                if (e.getMessage().split(" ")[1].equals("[[I"))
-                    for (int[] intArr : (int[][]) arr) {
-                        printArr(intArr);
-                        System.out.println();
-                    }
-            }
-        }
+//        if (arr.getClass().toString().split(" ")[1].split("")[0].equals("[")) {
+//            try {
+//                Object[][] c = (Object[][]) arr;
+//
+//
+//                int[] itc = ( (int[])(c[0][0]) );
+//
+//
+//
+//                System.out.println("cast");
+//                System.out.println( itc[0] );
+//                // Если не примитивный тип
+//                for (Object[] objArr : c) {
+//                    printArr(objArr);
+//                    System.out.println();
+//                }
+//
+//            } catch (ClassCastException e) { // если примитивный тип
+//                System.out.println("ClassCastException");
+//                if (e.getMessage().split(" ")[1].equals("[[I"))
+//                    for (int[] intArr : (int[][]) arr) {
+//                        printArr(intArr);
+//                        System.out.println();
+//                    }
+//
+//            }
+//        }
     }
 
 }
